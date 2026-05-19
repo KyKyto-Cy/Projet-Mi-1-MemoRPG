bc011eab31274be9322569fce17ca6c726e8ba14
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-19 10:44:03 +0200

    save.h

--- src/save.h
+++ src/save.h
@@ -1,6 +1,19 @@
 #ifndef SAVE_H
 #define SAVE_H
-
+#include "player.h"
 // TODO: prototypes des fonctions du module save
+
+typedef struct {
+    char nom[50];
+    int parties_jouees;
+    int victoires;
+} StatJoueur;
+
+
+void sauvegarder_stats(StatJoueur stats[], int nb_joueurs);
+void charger_stats(StatJoueur stats[], int *nb_joueurs);
+void afficher_stats(StatJoueur stats[], int nb_joueurs);
+
+
 
 #endif
