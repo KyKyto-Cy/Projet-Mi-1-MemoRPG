7237d2de88a5be1d12bd17fd0709d7e1bf1b6acf
KyKyto <turbakkyllian@gmail.com>
2026-05-21 21:44:39 +0000

    debut main

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "menu.h"
+#include "display.h"
+
+int main(void) {
+    int choix;
+
+    srand(time(NULL));
+
+    do {
+        choix = menu_principal();
+
+        if (choix == 1) {
+            Plateau plateau;
+            int choix_final;
+
+            choisir_joueur(&plateau);
+
+            do {
+                initialiser_plateau(&plateau);
+                afficher_plateau(&plateau);
+
+                choix_final = menu_final(&plateau);
+            } while (choix_final == 1);
+        }
+
+    } while (choix != 2);
+
+    return 0;
+}
