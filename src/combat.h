d34cbd01e4c343d337ebc23c7e105b51666f4498
bribrine <amb241106@gmail.com>
2026-05-23 18:16:52 +0200

    Ajout commentaire

--- src/combat.h
+++ src/combat.h
@@ -4,14 +4,19 @@
 #include "player.h"
 #include "cards.h"
 
+/* Retourne le nom du monstre */
 const char* NomMonstre(TypeMonstre monstre);
 
+/* Vérifie si une arme peut battre un monstre */
 int armeBatMonstre(armeActive arme, TypeMonstre monstre);
 
+/* Replace le joueur sur sa case de départ */
 void retourDepart(Joueur *joueur);
 
+/* Lance un combat entre un joueur et un monstre */
 void combat(Joueur *joueur, TypeMonstre monstre);
 
+/* Vérifie si le joueur a gagné */
 int joueurGagne(const Joueur *joueur);
 
 #endif
