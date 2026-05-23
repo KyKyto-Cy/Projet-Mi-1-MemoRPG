4d69f5c183257942637dc4f6ebe6ee458ee9023e
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-23 13:57:27 +0200

    commentaires game.h et include game.c

---
+++ src/game.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "board.h"
+#include "combat.h"
+#include "saisie.h"
+#include "player.h"
+#include "game.h"
+
+
+
--- src/game.h
+++ src/game.h
@@ -8,9 +8,12 @@
 #include "saisie.h"
 #include "player.h"
 
+//Vérifie si une case est accessible (adjacente et cachée)
+int case_accessible(Plateau *plateau, int ligne, int colonne); 
 
-int case_accessible(Plateau *plateau, int ligne, int colonne);
+//Déplace le joueur sur une case et la révèle
 void deplacer_joueur(Plateau *plateau, Joueur *joueur, int ligne, int colonne);
+// Gère un tour complet d'un joueur
 void tour_joueur(Plateau *plateau, Joueur *joueur);
 
 
